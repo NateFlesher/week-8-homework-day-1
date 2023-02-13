750b13d62e58ad2a9f5fdcaadb512332efee7308
Nate Flesher <you@example.comn8thegreat88@gmail.com>
2023-02-13 16:09:08 -0500

    Monday Commit

---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export const SignIn = () => {
+    return (
+        <div>
+            <h1>
+                Sign In Here
+            </h1>
+            </div >
+    )
+}
